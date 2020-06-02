48daadd8769be81432c6acf6f87386015ca11e54
thomasmeeus <thomas@sector7g.be>
2020-06-02 21:25:44 +0200

    Use underscore

--- example.tf
+++ example.tf
@@ -1,4 +1,4 @@
-resource "http-download" "readme_from_website" {
+resource "http_download" "readme_from_website" {
   remote_url    = "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.22/openshift-client-mac-4.1.22.tar.gz"
   filename      = "/tmp/openshift-client-mac-4.1.22.tar.gz"
   checksum      = "455234ae43a0a1a361ad474d785f0e1fadaac53d120f5444b610255fbe4f7a02"
--- provider.go
+++ provider.go
@@ -7,7 +7,7 @@ import (
 func Provider() *schema.Provider {
         return &schema.Provider{
                 ResourcesMap: map[string]*schema.Resource{
-                	"http-download": httpDownload(),
+					"http_download": httpDownload(),
                 },
         }
 }
