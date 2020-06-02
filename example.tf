dcf290c6699ac0bce2684c40b96ba77f4b3868d8
thomasmeeus <thomas@sector7g.be>
2020-06-02 22:20:11 +0200

    Update example

--- example.tf
+++ example.tf
@@ -1,4 +1,4 @@
-resource "httpdownload" "readme_from_website" {
+resource "httpdownload" "openshift-client-mac" {
   remote_url    = "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.22/openshift-client-mac-4.1.22.tar.gz"
   filename      = "/tmp/openshift-client-mac-4.1.22.tar.gz"
   checksum      = "455234ae43a0a1a361ad474d785f0e1fadaac53d120f5444b610255fbe4f7a02"
